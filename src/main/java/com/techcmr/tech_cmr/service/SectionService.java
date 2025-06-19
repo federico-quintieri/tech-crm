4719091905f58961ac3a28bdd086988ebd60487d
federico-quintieri <fedequinti98@gmail.com>
2025-06-19 11:31:18 +0200

    "Service for Section and Story"

--- src/main/java/com/techcmr/tech_cmr/service/SectionService.java
+++ src/main/java/com/techcmr/tech_cmr/service/SectionService.java
@@ -1,7 +1,62 @@
 package com.techcmr.tech_cmr.service;
 
+import com.techcmr.tech_cmr.dto.SectionDTO;
+import com.techcmr.tech_cmr.mapper.SectionMapper;
+import com.techcmr.tech_cmr.model.Section;
+import com.techcmr.tech_cmr.repository.SectionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
 public class SectionService {
+
+    @Autowired
+    private SectionRepository sectionRepository;
+
+    @Autowired
+    private SectionMapper sectionMapper;
+
+    // READ
+    public List<SectionDTO> findAllSections() {
+        return sectionRepository.findAll().stream().map(sectionMapper::toDto).toList();
+    }
+
+    // SHOW
+    public SectionDTO findSectionById(Long id) {
+        Section section = sectionRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return sectionMapper.toDto(section);
+    }
+
+    // CREATE
+    public SectionDTO createSection(SectionDTO sectionDTO) {
+        Section section = sectionMapper.toEntity(sectionDTO);
+        sectionRepository.save(section);
+        return sectionMapper.toDto(section);
+    }
+
+    // UPDATE
+    public void updateSection(Long id, SectionDTO sectionDTO) {
+
+        // Recupero la section esistente
+        Section existingSection = sectionRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        // Applico i nuovi valori dal DTO all'entità esistente
+        sectionMapper.updateEntityFromDto(sectionDTO, existingSection);
+
+        // Salvo l'entità aggiornata
+        sectionRepository.save(existingSection);
+    }
+
+    // DELETE
+    public void deleteSection(Long id) {
+        Section existingSection = sectionRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        sectionRepository.delete(existingSection);
+    }
 }
--- src/main/java/com/techcmr/tech_cmr/service/StoryService.java
+++ src/main/java/com/techcmr/tech_cmr/service/StoryService.java
@@ -1,7 +1,63 @@
 package com.techcmr.tech_cmr.service;
 
+import com.techcmr.tech_cmr.dto.StoryDTO;
+import com.techcmr.tech_cmr.mapper.StoryMapper;
+import com.techcmr.tech_cmr.model.Story;
+import com.techcmr.tech_cmr.repository.StoryRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class StoryService {
+
+    @Autowired
+    private StoryRepository storyRepository;
+
+    @Autowired
+    private StoryMapper storyMapper;
+
+    // READ
+    public List<StoryDTO> findAllStories() {
+        return storyRepository.findAll().stream().map(storyMapper::toDto).toList();
+    }
+
+    // SHOW
+    public StoryDTO findStoryById(Long id) {
+        // Trovo la story o lancio eccezione
+        Story story = storyRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Story not found"));
+        return storyMapper.toDto(story);
+    }
+
+    // CREATE
+    public StoryDTO createStory(StoryDTO storyDTO) {
+        // Da DTO ad entity
+        Story story = storyMapper.toEntity(storyDTO);
+        // Salvo story
+        storyRepository.save(story);
+        // La rimetto ad entity per il controller
+        return storyMapper.toDto(story);
+    }
+
+    // UPDATE
+    public void updateStory(Long id, StoryDTO storyDTO) {
+        // Trovo dal db la entity con determinato id
+        Story updatedStory = storyRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Story not found"));
+
+        // Merge entity con dto
+        storyMapper.updateEntityFromDto(storyDTO, updatedStory);
+
+        // La salvo nel db
+        storyRepository.save(updatedStory);
+
+    }
+
+    // DELETE
+    public void deleteStory(Long id) {
+        Story story = storyRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Story not found"));
+        storyRepository.delete(story);
+    }
+
 }
